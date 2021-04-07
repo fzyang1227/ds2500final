1b0f8d94de808106980bdd9c5aa5eecbe2ec4f88
Wesley Chung <chung.wes@northeastern.edu>
2021-04-06 22:33:19 -0400

    blurb_data

---
+++ blurb_data.py
@@ -0,0 +1,61 @@
+'''
+    DS2500 project
+    blurb_analysis
+    reads column, success/fail
+    counts most successful words, least successful words
+'''
+
+import pandas as pd
+import json
+import matplotlib.pyplot as plt
+
+STOPWORDS =["a", "an", "and", "the", "to", "i", "if", "of", "that", "it",
+            "is", "im", "has", "was", "his", "ive", "at", "in", "your", "its",
+            "for", "this", "are", "as", "be", "not", "have", "but", "with",
+            "by", "no", "dont", 'on', 'from', 'our', 'about']
+
+def read_csv():
+    
+    df = pd.read_csv('clean_data\\Kickstarter.csv')
+    csv_list = df.values.tolist()
+        
+    return csv_list
+
+def blurb(csv):
+    words = {}
+    for row in csv:
+        blurb = json.loads(row[1].replace('\'', '\"'))
+        for i in range(len(blurb)):
+            if blurb[i] in STOPWORDS:
+                continue
+            if blurb[i] not in words:
+                words[blurb[i]] = 0
+            if row[15] == 'successful':
+                words[blurb[i]] += 1
+            if row[15] == 'failed':
+                words[blurb[i]] -= 1
+    
+    sorted_words = dict(sorted(words.items(), key=lambda item: item[1], reverse=True))          
+    
+    return list(sorted_words.items())[:20]
+
+def visualize_blurb(word_dict):
+    x_list = []
+    y_list = []
+    for i in range(len(word_dict)):
+        x_list.append(word_dict[i][0])
+        y_list.append(word_dict[i][1])
+    plt.bar(x_list, y_list)
+    plt.title('Frequency of Words by Success/Failure, Top 20')
+    plt.xlabel('Word')
+    plt.xticks(rotation = 90)
+    plt.ylabel('Frequency')
+    for i in range(len(x_list)):
+        plt.annotate(str(y_list[i]), xy=(x_list[i],y_list[i]), ha='center', va='bottom')
+    plt.show()
+
+if __name__ == '__main__':
+    csv = read_csv()
+    word_dict = blurb(csv)
+    visualize_blurb(word_dict)
+    
