36c5c0627bc574413197c7819fd58a83d893eea7
Wesley Chung <chung.wes@northeastern.edu>
2021-04-20 11:31:43 -0400

    edited main

--- main.py
+++ main.py
@@ -10,20 +10,20 @@ from dectreeclassifer import *
 
 if __name__ == '__main__':
     # open_csv()
-    '''csv = read_csv()
+    csv = read_csv()
     word_dict = blurb(csv)
     visualize_blurb(word_dict)
     get_location_success_failure_data()
-    get_location_pledged_data()'''
+    get_location_pledged_data()
     
     dataset = read_csv_short()
     
     # plots max depth until 20
-    #dectreeplot(20, dataset)
+    dectreeplot(20, dataset)
     
     # highest accuracy, recall, f1-score png saved at max_depth = 3
-    #dectree(dataset)
+    dectree(3, dataset)
     
     # highest precision png saved at max_depth = 6
     # will overwrite last png, use saved pngs
-    dectree(dataset)
+    dectree(6, dataset)
